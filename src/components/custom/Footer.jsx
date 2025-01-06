49591f866230945f2150c975c55f5abd2413323d
Ansh Lakhera <lakheraansh@gmail.com>
2025-01-07 02:10:45 +0530

    Footer changes Updated

--- src/components/custom/Footer.jsx
+++ src/components/custom/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="flex items-center justify-center pt-5 pb-3 w-full bottom-0">
-      Made with ❤️ by Nikshay Gupta
+      Made with ❤️ by Ansh Lakhera
     </div>
   );
 };
